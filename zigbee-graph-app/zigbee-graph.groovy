fedb3d9106e9a14667618aaa22cf13285b6a2f93
Agamemnon Dan Danache <dan-danache@users.noreply.github.com>
2023-05-22 18:14:00 +0300

    Update zigbee-graph.groovy
    Fix whitespace

--- zigbee-graph-app/zigbee-graph.groovy
+++ zigbee-graph-app/zigbee-graph.groovy
@@ -7,16 +7,16 @@
  * https://github.com/sburke781/hubitat/blob/master/SimpleCSSEditor/SimpleCSSEditor_driver.groovy
  */
 definition(
-	name: "Zigbee Graph",
-	namespace: "dandanache",
-	author: "Dan Danache",
-	description: "Eye-candy visual render for data we get from /hub/zigbee/getChildAndRouteInfo",
-	category: "Utility",
-	singleInstance: true,
-	iconUrl: "",
-	iconX2Url: "",
-	oauth: false,
-	importUrl: "https://raw.githubusercontent.com/dan-danache/hubitat/master/zigbee-graph-app/zigbee-graph.groovy"
+    name: "Zigbee Graph",
+    namespace: "dandanache",
+    author: "Dan Danache",
+    description: "Eye-candy visual render for data we get from /hub/zigbee/getChildAndRouteInfo",
+    category: "Utility",
+    singleInstance: true,
+    iconUrl: "",
+    iconX2Url: "",
+    oauth: false,
+    importUrl: "https://raw.githubusercontent.com/dan-danache/hubitat/master/zigbee-graph-app/zigbee-graph.groovy"
 )
 
 /**********************************************************************************************************************************************/
@@ -26,13 +26,13 @@ private htmlFileSrc() { return "https://raw.githubusercontent.com/dan-danache/hu
 private htmlFileDst() { return "zigbee-graph.html" }
 /**********************************************************************************************************************************************/
 preferences {
-	page name: "mainPage"
+    page name: "mainPage"
 }
 
 def mainPage() {
-	dynamicPage (name: "mainPage", title: "Zigbee Graph - v${releaseVer() + ' - ' + appVerDate()}", install: true, uninstall: true) {
+    dynamicPage (name: "mainPage", title: "Zigbee Graph - v${releaseVer() + ' - ' + appVerDate()}", install: true, uninstall: true) {
         if (app.getInstallationState() != "COMPLETE") {
-		    section {
+            section {
                 label title: "Once you click the [Done] button, we will try to retrieve the 'zigbee-graph.html' from Github and store it in the File Manager."
             }
         } else {
@@ -83,23 +83,23 @@ String readExtFile(fName){
     try {
         httpGet(params) { resp ->
             if (resp!= null) {
-               int i = 0
-               String delim = ""
-               i = resp.data.read() 
-               while (i != -1){
-                   char c = (char) i
-                   delim+=c
-                   i = resp.data.read() 
-               }
-               log.trace "File ${fName} was successfully downloaded"
-               return delim
+                int i = 0
+                String delim = ""
+                i = resp.data.read() 
+                while (i != -1){
+                    char c = (char) i
+                    delim+=c
+                    i = resp.data.read() 
+                }
+                log.trace "File ${fName} was successfully downloaded"
+                return delim
             }
             else {
-                errorLog("Null Response")
+                log.error "Null Response"
             }
         }
     } catch (exception) {
-        errorLog("Read Ext Error: ${exception.message}")
+        log.error "Read Ext Error: ${exception.message}"
         return null;
     }
 }
@@ -110,11 +110,11 @@ Boolean writeFile(String fName, String fData) {
     now = new Date()
     String encodedString = "thebearmay$now".bytes.encodeBase64().toString();    
     try {
-		def params = [
-			uri: 'http://127.0.0.1:8080',
-			path: '/hub/fileManager/upload',
-			query: [ 'folder': '/' ],
-			headers: [ 'Content-Type': "multipart/form-data; boundary=$encodedString" ],
+        def params = [
+            uri: 'http://127.0.0.1:8080',
+            path: '/hub/fileManager/upload',
+            query: [ 'folder': '/' ],
+            headers: [ 'Content-Type': "multipart/form-data; boundary=$encodedString" ],
             body: """--${encodedString}
 Content-Disposition: form-data; name="uploadFile"; filename="${fName}"
 Content-Type: text/plain
@@ -126,17 +126,17 @@ Content-Disposition: form-data; name="folder"
 
 
 --${encodedString}--""",
-			timeout: 500,
-			ignoreSSLIssues: true
-		]
+            timeout: 500,
+            ignoreSSLIssues: true
+        ]
 
-		httpPost(params) { resp ->
-		}
+        httpPost(params) { resp ->
+        }
         log.trace "File Manager entry ${fName} successfully saved"
-		return true
-	}
-	catch (e) {
-		log.error "Error writing file $fName: ${e}"
-	}
-	return false
+        return true
+    }
+    catch (e) {
+        log.error "Error writing file $fName: ${e}"
+    }
+    return false
 }
